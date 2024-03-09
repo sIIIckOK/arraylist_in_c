39275691c6067551ca6a72fd6bcc85b90c2c5122
siiick <sid@DESKTOP-4I5MGPL>
2024-03-09 22:57:41 +0545

    Change function name Add namespace

--- example.c
+++ example.c
@@ -12,33 +12,33 @@ typedef struct {
 int main(){
     //CreateArrayList: creates array list
     //ArrayList*(int number_of_elements, ...elements)<variadic>
-    ArrayList* a = CreateArrayList(3, 1, 2, 3);
+    ArrayList* a = AL_CreateArrayList(3, 1, 2, 3);
     printf("Created an Array List: \n");
 
     //PrintArrayList: prints the array list
-    PrintArrayList(a);
+    AL_PrintArrayList(a);
 
     //AppendArrayList: appends an element to an array list
     //void (ArrayList*, int v)
-    AppendArrayList(a, 4);
+    AL_AppendArrayList(a, 4);
     printf("Appended 4 to an Array List: \n");
-    PrintArrayList(a);
+    AL_PrintArrayList(a);
 
     //ExtendArrayList: extend multiple elements to an array list
     //void (ArrayList*, int number_of_elements, ...elements)<variadic>
     //automatically re-allocates memory if needed
-    ExtendArrayList(a, 3, 5, 6, 7);
+    AL_ExtendArrayList(a, 3, 5, 6, 7);
     printf("Appended (5, 6, 7) an Array List: \n");
-    PrintArrayList(a);
+    AL_PrintArrayList(a);
 
     //DeleteElement: delete and element from array list (by index)
     //void (ArrayList*, int index_of_element)
-    DeleteElement(a, 3);
+    AL_DeleteElement(a, 3);
     printf("Appended (5, 6, 7) an Array List: \n");
-    PrintArrayList(a);
+    AL_PrintArrayList(a);
 
     //ReEvalArrayList: revaluates the linked lists 
     //removes extra unused space in the list
     //void 
-    ReEvalArrayList(a);
+    AL_ReEvalArrayList(a);
 }
